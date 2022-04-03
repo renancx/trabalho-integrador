99856587fa406491471c51e8ef9d1c148df4bca9
Renan <renan.lowe@hotmail.com>
2022-04-03 15:02:42 -0300

    create page to control finances

--- Programacao2/financeiro.php
+++ Programacao2/financeiro.php
@@ -10,25 +10,38 @@
     </head>
     <body>
         <!--barra de navegacao-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
+            <span class="navbar-toggler-icon" onclick="window.location.href='index.html' "></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link" href="lotes.php">Situação dos Lotes</a>
+                    </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="lotes.php">Situação dos Lotes</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="estoque.php">Consultar Estoque</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="financeiro.php">Relatório Financeiro</a>
-                  </li>
-                </ul>
-              </div>
+                        <a class="nav-link" href="estoque.php">Consultar Estoque</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="financeiro.php">Controle Financeiro</a>
+                    </li>
+                    </ul>
+                </div>
             </div>
         </nav>
         <!--------------------->
+        <h1>Controle Financeiro</h1>
+            <div id="consulta" class="border">
+                <p>Consultar relatório financeiro:</p>
+                <a href="./financeiro/extrato.php">Visualizar</a>
+            </div><br>
+            <div id="venda" class="border">
+                <p>Registrar venda:</p>
+                <a href="./financeiro/venda.php">Clique aqui</a>
+            </div><br>
+            <div id="compra" class="border">
+                <p>Registrar compra:</p>
+                <a href="./financeiro/compra.php">Clique aqui</a>
+            </div>
         </body>
 </html>
