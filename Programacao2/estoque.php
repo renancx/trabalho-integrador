a959f7217439915794aecab36e69a855a2291307
Renan <renan.lowe@hotmail.com>
2022-04-03 15:03:02 -0300

    update some infos

--- Programacao2/estoque.php
+++ Programacao2/estoque.php
@@ -10,9 +10,9 @@
     </head>
     <body>
         <!--barra de navegacao-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon" onclick="window.location.href='index.html' "></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav">
@@ -23,7 +23,7 @@
                       <a class="nav-link active" aria-current="page" href="estoque.php">Consultar Estoque</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="financeiro.php">Relatório Financeiro</a>
+                      <a class="nav-link" href="financeiro.php">Controle Financeiro</a>
                   </li>
                 </ul>
               </div>
@@ -44,7 +44,8 @@
                 <p>Capacidade: <span id="cap_silo2"></span></p>
 
             </div>
-            <a href="gestao.php">Gerenciar Silos</a>
+            <a href="gestao.php">Gerenciar Silos</a><br>
+            <a href="index.html">Voltar</a>
         </div>
     </body>
 </html>
