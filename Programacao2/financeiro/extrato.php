56779f85880ec463d1190ad0d6fc44ad5ed188f9
Renan <renan.lowe@hotmail.com>
2022-04-04 19:58:14 -0300

    include informations

---
+++ Programacao2/financeiro/extrato.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Extrato Financeiro</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Relatório Financeiro</h1>
+        <h2>Extrato:</h2>
+        <div class="border">
+            
+        </div>
+        <a href="/financeiro.php">Voltar</a>
+    </body>
+</html>
---
+++ Programacao2/financeiro/venda.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registrar compra</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Registrar venda</h1>
+        <h2>Selecione as opções:</h2>
+        <div class="border">
+            <h3>Informações</h3>
+            <p>Categoria: <span>Sementes</span></p>
+            <p>Data: <span>04/04/2022</span></p>
+            <p>Valor: R$<span>500</span>,00</p>
+        </div><br>
+        <a href="/financeiro.php">Voltar</a>
+    </body>
+</html>
