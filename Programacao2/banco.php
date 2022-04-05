776b1d28ff2cf5fea7ce63d79bde12006d1ed573
Renan <renan.lowe@hotmail.com>
2022-04-05 00:49:29 -0300

    connect the db

--- Programacao2/banco.php
+++ Programacao2/banco.php
@@ -1,5 +1,30 @@
 <?php
 
-    
+    $cn = pg_connect("host=localhost port=5432 dbname=integrador user=postgres password=12345");
 
+    //if($cn) {
+    // echo 'connected';
+    //}
+
+    $result = pg_query($cn, "select * from proprietario");
+    while($row = pg_fetch_object($result)) {
+        echo $row->nome_prop;
+    }
+    pg_close($cn);
+    /*
+    $user = 'postgres';
+    $password = '12345';
+
+    try{
+
+        $myPDO = new PDO('psql:host=localhost; dbname=integrador', $user, $password);
+
+        if($myPDO) {
+            echo 'Connected to postgres';
+        }
+
+    } catch(PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+    */
 ?>
