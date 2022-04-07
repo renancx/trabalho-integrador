a543c07844a000c36452b360011e70b8198dfb3f
Renan <renan.lowe@hotmail.com>
2022-04-07 00:33:05 -0300

    update index

--- Programacao2/index.php
+++ Programacao2/index.php
@@ -12,34 +12,34 @@
     <body>
         <!--barra de navegacao-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-success"></nav><br>
-
+        <!---------------------->
         <h1 class="container text-center">Bem vindo, <span>Proprietário</span></h1><hr>
         <div class="container text-center">
-            <h5 class="bg-success text-white rounded">Gerencie a sua propriedade:</h5>
+            <h5 class="bg-success text-white rounded p-2 border-dark border">Gerencie a sua propriedade:</h5>
         </div>
 
         
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div class="card-header bg-success text-white">Gestão dos lotes</div>
-                    <div class="card-body">
+                    <div class="card-header bg-success text-white border border-dark">Gestão dos lotes</div>
+                    <div class="card-body border border-dark">
                         <h5 class="card-title">Monitoramento</h5>
                         <p class="card-text">Consulte e controle a situação dos lotes de suínos de sua propriedade.</p>
                         <a href="lotes.php" class="btn btn-success">Clique aqui</a>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card-header bg-success text-white">Gerenciar Estoque</div>
-                    <div class="card-body">
+                    <div class="card-header bg-success text-white border border-dark">Gerenciar Estoque</div>
+                    <div class="card-body border border-dark">
                         <h5 class="card-title">Administração</h5>
                         <p class="card-text">Controle o estoque de ração de sua propriedade.</p>
                         <a href="estoque.php" class="btn btn-success">Clique aqui</a>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card-header bg-success text-white">Situação Financeiro</div>
-                    <div class="card-body">
+                    <div class="card-header bg-success text-white border border-dark">Situação Financeiro</div>
+                    <div class="card-body border border-dark">
                         <h5 class="card-title">Controle</h5>
                         <p class="card-text">Gerencie as compras e vendas realizadas na propriedade.</p>
                         <a href="financeiro.php" class="btn btn-success">Clique aqui</a>
@@ -52,7 +52,7 @@
         <!--Mostrar a data atual-->
         <br><br>
         <div class="border card container text-center w-25 bg-success text-white border-dark">
-            <h3>Data de hoje:</h3>
+            <h3>Data de Hoje:</h3>
             <div id="current_date">
                 <script>
                     date=new Date();
