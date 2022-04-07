985fbdcbc4adb60428beeff3dc84094f5820f38e
Renan <renan.lowe@hotmail.com>
2022-04-07 02:48:18 -0300

    update page layout

--- Programacao2/novolote.php
+++ Programacao2/novolote.php
@@ -1,29 +1,33 @@
 <?php
-    //require_once 'banco.php';
+    require_once 'banco.php';
 
+    /*
+    session_start();
+    try {
+        if(isset($_POST['enviar'])) {
 
-    
-    if(isset($_POST['btnSave']) && $_POST['btnSave']=='Save') {
+            $idlote=$_POST['id_lote'];
+            $tamanho=$_POST['tam_lote'];
+            $data_cheg=$_POST['data_cheg_lote'];
+            $data_sair=$_POST['data_venda_lote'];
+            $vendido=$_POST['vendido_lote'];
 
-        $cn = pg_connect("host=localhost port=5432 dbname=integrador user=postgres password=12345");
+            $comando = $db->prepare('INSERT INTO lote (id_lote, tam_lote, data_cheg_lote, data_venda_lote, vendido_lote) VALUES (:id_lote, :tam_lote, :data_cheg_lote, :data_venda_lote, :vendido_lote)');
+            
+            $comando->bindParam(':id_lote', $idlote);
+            $comando->bindParam(':tam_lote', $tamanho);
+            $comando->bindParam(':data_cheg_lote', $data_cheg);
+            $comando->bindParam(':data_venda_lote', $data_sair);
+            $comando->bindParam(':vendido_lote', $vendido);
+            $comando->execute();
 
-        $result = pg_query($cn, "SELECT * FROM lote");
-
-        $idlote=$_POST['idlote'];
-        $tamanho=$_POST['tam'];
-        $data_cheg=$_POST['cdata'];
-        $data_sair=$_POST['vdata'];
-        $vendido=$_POST['vendido'];
-
-        $query="call add_product('.$idlote.','.$tamanho.','.$data_cheg.','.$data_sair.','.$vendido.')";
-        $res = pg_query($cn,$query);
-        if($res) {
-            echo 'Saved';
-        }
-        else {
-            echo 'error';
+            echo 'deu bom (eu acho)';
         }
     }
+    catch (PDOException $e) {
+        echo 'Erro ao executar comando no banco de dados: ' . $e->getMessage();
+        exit();
+    }*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,13 +36,20 @@
         <title>Adicionar novo lote</title>
     </head>
     <body>
-        <h1>Adicionar novo lote</h1>
-        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-        <span>ID do lote:  </span><input type="text" name="id_lote"><br><br>
-        <span>Tamanho do lote:  </span><input type="text" name="tam_lote"><br><br>
-        <span>Data de chegada:  </span><input type="text" name="data_cheg_lote"><br><br>
-        <span>Data de saída:  </span><input type="text" name="data_saida_lote"><br><br>
-        <span>Vendido?  </span><input type="text" name="vendido_lote"><br>
-        <input type="submit" values="Save" name="btnSave">
+        <!--barra de navegacao-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success"></nav><br>
+        <!---------------------->
+        <h1 class="container text-center">Adicionar novo lote</h1><hr><br>
+        <div class="card bg-success text-white rounded-bottom container text-center">
+            <form action="novolote.php" method="post">
+                <span>ID do lote:</span><br><input type="text" name="id_lote"><br><br>
+                <span>Tamanho do lote:</span><br><input type="text" name="tam_lote"><br><br>
+                <span>Data de chegada:</span><br><input type="text" name="data_cheg_lote"><br><br>
+                <span>Data de saída:</span><br><input type="text" name="data_saida_lote"><br><br>
+                <span>Vendido?</span><br><input type="text" name="vendido_lote"><br><br>
+                <input type="submit" values="enviar" name="enviar"><br>
+            </form>
+            <a href="http://localhost/trabalho-integrador/Programacao2/lotes.php" class="btn btn-success text-center">Cancelar</a>
+        </div>
     </body>
 </html>
