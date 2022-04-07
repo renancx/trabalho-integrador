009c8033f388d5971933e71665f63dd0e0a9a3d7
Renan <renan.lowe@hotmail.com>
2022-04-07 03:42:07 -0300

    update design from file

--- Programacao2/lotes/lote2.php
+++ Programacao2/lotes/lote2.php
@@ -7,18 +7,23 @@
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <title>Lote 2</title>
+        <title>Lote</title>
     </head>
     <body>
-        <h1>Lote selecionado:</h1>
-        <p class="border">Lote 2</p>
-        <div class=border>
-            <p>ID: <span>1234</span></p>
-            <p>Data de chegada: <span>02/04/2022</span></p>
-            <p>Quantidade de suínos: <span>200 animais</span></p>
-            <p>Data da vacina: <span>08/07/2022</span></p>
-            <p>Data de saída: <span> 06/05/2022</span></p>
+        <!--barra de navegacao-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success"></nav><br>
+        <!---------------------->
+        <h1 class="text-center container">Atualizar informações do lote:</h1><hr><br>
+
+        <div class="container text-center border bg-success text-white">
+            <form method="post">
+                <span>Data de chegada</span><br><input type="text"><br><br>
+                <span>Data de saída</span><br><input type="text"><br><br>
+                <span>Tamanho do lote</span><br><input type="text"><br><br>
+                <input type="checkbox"><span> Vacina aplicada</span><br><br>
+                <input type="submit" values="enviar"><br>
+            </form>
+            <a href="http://localhost/trabalho-integrador/Programacao2/lotes.php" class="text-center btn btn-success">Voltar</a>
         </div>
-        <a href="http://localhost/trabalho-integrador/Programacao2/lotes.php">Voltar</a>
     </body>
 </html>
