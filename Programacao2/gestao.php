7403f14959d81190f9ac51b6d671c679fab7f550
Renan <renan.lowe@hotmail.com>
2022-04-07 03:01:02 -0300

    update page style

--- Programacao2/gestao.php
+++ Programacao2/gestao.php
@@ -10,21 +10,24 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Alterar estoque dos silos:</h1>
-        
-        <h2>Selecione o silo:</h2><br>
-        <div class="border">
-            <h3>Tipo da ração: <span id="tipo_silo1">Adulto</span></h3>
-            <p>Quantidade adicionada:</p>
-            <p>Quantidade removida:</p>
+        <!--barra de navegacao-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success"></nav><br>
+        <!---------------------->
+        <h1 class="container text-center">Alterar estoque dos silos:</h1><hr><br>
+        <div class="container text-center border bg-success text-white">
+            <form>
+                <fieldset>
+                    <legend>Selecione o tipo da ração:</legend>
+                    <input list="opcoes" name="opcoes">
+                    <datalist id="opcoes">
+                        <option value="Adulto">
+                        <option value="Filhote">
+                        <option value="Especial">
+                    </datalist>
+                </fieldset>
+                <span>Quantidade adicionada:</span><br><input type="text"><br><br>
+            </form>
+            <a href="estoque.php" class="container text-center text-dark">Voltar</a>
         </div>
-
-        <div class="border">
-            <h3>Tipo da ração: <span id="tipo_silo2">Filhote</span></h3>
-            <p>Quantidade adicionada:</p>
-            <p>Quantidade removida:</p>
-        </div>
-
-        <a href="estoque.php">Voltar</a>
     </body>
 </html>
